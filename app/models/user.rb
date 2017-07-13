26533efbde2cbbef2289adab8f88ba9370168581
horoama <horoama929@gmail.com>
2017-07-13 19:36:34 +0900

    add relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   attr_accessor :login
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following,  through: :active_relationships,  source: :followed
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers,  through: :passive_relationships,  source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -17,5 +21,20 @@ class User < ApplicationRecord
     else
         where(conditions).first
     end
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+      active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+      following.include?(other_user)
   end
 end
