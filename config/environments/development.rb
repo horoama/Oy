ce02733c3f3f2f45d3c8df14188ba053f4843dc5
horoama <horoama929@gmail.com>
2018-03-02 13:43:14 +0900

    change smtp_settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -52,6 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost',  port: 3000 }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
       :address => "smtp.gmail.com",
@@ -59,6 +60,6 @@ Rails.application.configure do
       :domain => 'smtp.gmail.com',
       :user_name => ENV["GMAIL_ADDRESS"],  #gmailアドレス
       :password => ENV["GMAIL_PASSWORD"],  #gmailパスワード
-      :authentication => 'login',
+      :authentication => 'plain',
   }
 end
